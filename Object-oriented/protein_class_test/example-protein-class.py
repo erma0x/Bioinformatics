cfc61fcb9d871355eb4d9cd6d3bc9232f6d2049d
Moebius <35337869+ErmanoBuikis@users.noreply.github.com>
2021-03-18 15:34:48 +0100

    obj oriented

---
+++ Object-oriented/protein_class_test/example-protein-class.py
@@ -0,0 +1,31 @@
+class proteine:
+    def composizione(self,lunghezza=0,peso=0):
+        
+        self.lunghezza=lunghezza
+        self.peso=peso
+        
+hb=proteine(lunghezza=121,peso=312,composizione=['a','b'],funzione='ce')
+print hb
+    def input_composizione():
+        nome=str(input('inserisci il nome della proteina'))
+        listaproteine.append(nome)
+        lunghezza=input('inserisci lunghezza amminoacidica proteina')
+        listalunghezzeproteine.append(lunghezza)
+        peso=input('inserisci peso in u.m.a. della proteina')
+    
+        
+    def assegnavalori(self):    
+        nomep=input('inserisci il nome della proteina')
+        lunghezzap=int(input('inserisci lunghezza amminoacidica proteina'))
+        pesop=int(input('inserisci peso in u.m.a. della proteina'))
+        nomep=proteine(self=nomep,lunghezza=lunghezzap,peso=pesop)
+        print proteine(nomep)
+
+
+proteine.assegnavalori(self)
+print '---'
+
+
+
+    
+
